81834ed3d8ff21fd22c2553648e1c458b759252d
LeMoineau <faberbastian@gmail.com>
2020-12-18 20:38:08 +0100

    improve index.html & begin Register.vue

--- client/components/Register.vue
+++ client/components/Register.vue
@@ -1,26 +1,17 @@
 <template>
-    <div class="main">
-        <div class="conta">
-            <form v-on:submit.prevent="inscription(name, pw)">
-                <div class="title">
-                    <h2>Inscription</h2>
-                </div>
-                <div class="reponse">
-                    <label for="email">Email</label>
-                    <input v-on:input="name = $event.target.value" type="text" required>
-                    <label for="">Password</label>
-                    <input v-on:input="pw = $event.target.value" type="password" name="password" required >
-                    <input type="submit" value="Inscription">
-                </div>
-            </form>
-            <p>si tu as déjà un compte connecte toi <a v-bind:href="'#/Login'">ici</a></p>
-        </div>
+    <div id="register-container">
+      <div class="register-subcontainer">
+
+      </div>
+      <div class="register-subcontainer">
+
+      </div>
     </div>
 </template>
 
 <script>
 module.exports = {
-    props: {
+  props: {
 
   },
   data () {
@@ -29,10 +20,10 @@ module.exports = {
       pw : "",
     }
   },
-    methods: {
-        inscription(email,password){
-            this.$emit("inscription",email, password)
-        }
-    }
+  methods: {
+      inscription(email,password){
+          this.$emit("inscription",email, password)
+      }
+  }
 }
 </script>
--- client/vue-application.js
+++ client/vue-application.js
@@ -22,93 +22,13 @@ var app = new Vue({
       prenom: "pierrot",
       nom: "le ouf",
       icon: "https://images-na.ssl-images-amazon.com/images/I/51CDY-aeXvL._SX486_BO1,204,203,200_.jpg",
-      isRegistered: true
+      isRegistered: false
     }
   },
   async mounted () {
-    await axios.post('/api/code')
+    //await axios.post('/api/code')
   },
   methods: {
-    async addArticle (article) {
-      const res = await axios.post('/api/article', article)
-      this.articles.push(res.data)
-      location.replace("#/home")
-    },
-    async updateArticle (articleId) {
-      if (articleId !== -1) {
-        location.replace('#/updateArticle?id=' + articleId);
-      }
-    },
-    async deleteArticle (articleId) {
-      const index = this.articles.findIndex(a => a.id === articleId)
-      if (index !== -1) {
-          this.articles.splice(index, 1)
-          await axios.delete('/api/article/' + articleId)
-      }
-    },
-    async inscription(mail,pass){
-      const res = await axios.post('/api/register' , {email: mail ,password: pass})
-      let result = res.data.message
-      if(result === "Registed"){
-        location.replace("#/home")
-      }else{
-        alert("Ce compte existe déjà")
-      }
-    },
-    async login(mail,pass){
-      const res = await axios.post('/api/login', {email: mail, password: pass})
-      let result = res.data.message
-      if(result === "Connected"){
-        location.replace("#/home")
-        this.users.userId = res.data.userId
-      }
-      else if(result === 'AlreadyConnected'){
-        alert("Vous êtes déjà connecté")
-        this.users.userId = res.data.userId
-      }
-      else{
-        alert("Mot de passe oublié veuillez vous réinscrire")
-      }
-    },
-    async loadArticleView(articleId){
 
-      return this.articles.find(c => c.id === articleId)
-    },
-    async checkPerm(callblack) {
-      const res = await axios.get("/api/me")
-      this.users.userId = res.data.message
-      const res1 = await axios.get("/api/perm")
-      if (res1.data.message.rows.length > 0) {
-        this.users.userPerm = res1.data.message.rows[0].perm
-      }
-      const res2 = await axios.get("/api/name")
-      if (res2.data.message.rows.length > 0) {
-        this.users.userName = res2.data.message.rows[0].email
-      }
-      callblack(res.message);
-    },
-    async disconnect() {
-      await axios.post("/api/disconnect")
-      console.log("disconnected");
-      this.users = {
-        userName: "none",
-        userId: -1,
-        userPerm: 0
-      }
-    },
-    async getArticleInfos(articleId) {
-      const res = await axios.get("/api/article/" + articleId);
-      if (res.data.message.rows.length > 0) {
-          this.selected = res.data.message.rows[0];
-      }
-    },
-    async editArticle(article) {
-      console.log(article)
-      const res = await axios.put("/api/article", {id: article.id, title: article.title, text: article.text, image: article.image, creator: article.creator});
-      let editingIndex = this.articles.findIndex(a => a.id === parseInt(article.id));
-      this.articles[editingIndex].title = article.title;
-      this.articles[editingIndex].creator = article.creator;
-      this.articles[editingIndex].image = article.image;
-    }
   }
 })
